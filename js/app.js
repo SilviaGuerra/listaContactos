a612d1170fde999d9a107a4a1e22b18d4e84da24
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-11-30 17:11:08 -0600

    imprime nombre, apellido y usuario

--- js/app.js
+++ js/app.js
@@ -14,15 +14,21 @@ var loadUser = function(){
 }
 
 var contactList = function(contact){
-  var firtsName = contact.name,first;
+  var firtsName = contact.name.first;
+  var lastName = contact.name.last;
   var userName = contact.login.username;
   var picture = contact.picture.large;
   var location = contact.location.street;
   var email = contact.email;
   var cell = contact.cell;
 
-  var template = '<div class="row">'+ '<div class="col-6 card">' +
-                 '<div>' + '<img src='+ picture + '>' + '</div>' +
+  var template = '<div class="row">'+
+                  '<div class="col-6 card">' +
+                    '<div>' +
+                      '<img src='+ picture + '>' +
+                    '</div>' +
+                    '<div class="name">' + firtsName + ' ' + lastName +'</div>' +
+                    '<div class="userName">' + '@' + userName +'</div>' +
                  '</div>'+ '<div class="col-6 card">' + '<div>' +
                  '<img src='+ picture + '>' + '</div>' + '</div>'+ '</div>'
 
